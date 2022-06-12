236110b3b12f5a1795739dd2dc92c8ca3c7e0869
Nathan Hensel <nathanihensel@gmail.com>
2022-06-12 12:23:57 -0500

    explain the .3

--- ups-metrics_runner/snmp.py
+++ ups-metrics_runner/snmp.py
@@ -9,6 +9,7 @@ load = session.get('.1.3.6.1.2.1.33.1.4.4.1.5.1')
 ac_voltage = session.get('.1.3.6.1.2.1.33.1.3.3.1.3.1')
 battery_temp = session.get('.1.3.6.1.2.1.33.1.2.7.0')
 
+# 300 watt device
 wattage = int(float(load.value)*0.3)
 
 env = os.environ.copy()
