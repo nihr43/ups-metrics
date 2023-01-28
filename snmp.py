537ba9bc5226aa233b661fffd2cfbec3d8e87def
nathan hensel <nathanihensel@gmail.com>
2023-01-27 22:31:56 -0600

    clean up mess, pull tripplite spec into class

--- snmp.py
+++ snmp.py
@@ -2,36 +2,35 @@ import os
 
 from easysnmp import Session
 from flask import Flask
-from flask import jsonify
 from flask_restful import Resource, Api
+
+
+class Tripplite_Smart:
+    def __init__(self, ip):
+        self.ip = ip
+        session = Session(hostname=self.ip, community='public', version=1)
+
+        # values discovered by manually inspecting
+        #  `snmpwalk -v 1 -c public 10.0.0.143`
+        # the leading 'iso.' is replaced with '.1.' because... science?
+
+        # SMART500RT1U is a 300 watt device, raw value appears to be x/1000
+        load_raw = session.get('.1.3.6.1.2.1.33.1.4.4.1.5.1')
+        self.load = int(float(load_raw.value)*0.3)
+
+        uptime_raw = session.get('.1.3.6.1.2.1.1.3.0')
+        self.uptime_days = int(float(uptime_raw.value)/8640000)
+
+        self.ac_voltage = int(session.get('.1.3.6.1.2.1.33.1.3.3.1.3.1').value)
+        self.battery_temp = int(session.get('.1.3.6.1.2.1.33.1.2.7.0').value)
+        self.mac = session.get('.1.3.6.1.2.1.2.2.1.6.2').value
+        self.model = session.get('.1.3.6.1.2.1.33.1.1.2.0').value
 
 
 class battery(Resource):
     def get(self):
-        session = Session(hostname=os.environ.get('SNMP_ADDRESS'),
-                          community='public', version=1)
-
-        # values discovered by manually inspecting
-        #  `snmpwalk -v 1 -c public 10.0.0.143`
-        # here the leading 'iso.' is replaced with '.1.' because... science?
-        load_raw = session.get('.1.3.6.1.2.1.33.1.4.4.1.5.1')
-        # this is a 300 watt device, raw value appears to be x/1000
-        load = int(float(load_raw.value)*0.3)
-        ac_voltage = session.get('.1.3.6.1.2.1.33.1.3.3.1.3.1')
-        battery_temp = session.get('.1.3.6.1.2.1.33.1.2.7.0')
-        mac = session.get('.1.3.6.1.2.1.2.2.1.6.2')
-        uptime_raw = session.get('.1.3.6.1.2.1.1.3.0')
-        uptime_days = int(float(uptime_raw.value)/8640000)
-        model = session.get('.1.3.6.1.2.1.33.1.1.2.0')
-
-        vals = []
-        vals.append({'load': load,
-                     'ac_voltage': ac_voltage.value,
-                     'battery_temp': battery_temp.value,
-                     'uptime_days': uptime_days,
-                     'model': model.value})
-
-        return(jsonify(ups=vals))
+        ups = Tripplite_Smart(os.getenv('SNMP_ADDRESS'))
+        return(ups.__dict__)
 
 
 def main():
